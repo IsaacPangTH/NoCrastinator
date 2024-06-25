04644abbf3058c34c6950878a9efe448b99e08cc
Isaac Pang <isaac.pangth@gmail.com>
2024-06-25 23:50:11 +0900

    instaled and set up react-big-calendar and date-fns

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import enUS from "date-fns/locale/en-US";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
 
 export default function TaskCalendar() {
-  return <></>;
+  return (
+    <>
+      <Calendar localizer={localizer} />
+    </>
+  );
 }
