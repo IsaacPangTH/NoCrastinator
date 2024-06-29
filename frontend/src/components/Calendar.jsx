5c39578ef5d47126f8a703fdefcc9804e0ea8072
Isaac Pang <isaac.pangth@gmail.com>
2024-06-29 14:02:25 +0900

    added events to calendar

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -5,6 +5,7 @@ import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
 import getDay from "date-fns/getDay";
 import enUS from "date-fns/locale/en-US";
+import { parseISO } from "date-fns";
 
 const locales = {
   "en-US": enUS,
@@ -19,12 +20,24 @@ const localizer = dateFnsLocalizer({
 });
 
 export default function TaskCalendar() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([
+    //test task object - to be removed when database and backend is linked
+    { title: "test", startTime: "2024-06-29T15:00:00", endTime: "2024-06-29T16:00:00" },
+  ]);
 
-  const events = tasks.filter((task) => task.start !== null && task.end !== null);
+  const events = tasks
+    .filter((task) => task.startTime !== null && task.endTime !== null)
+    .map((task) => {
+      return { title: task.title, start: parseISO(task.startTime), end: parseISO(task.endTime) };
+    });
   return (
     <>
-      <Calendar localizer={localizer} />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        defaultView="week"
+        views={["month", "week", "day"]}
+      />
     </>
   );
 }
