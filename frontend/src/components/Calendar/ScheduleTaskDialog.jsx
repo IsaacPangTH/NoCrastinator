7a2036dcddeecac1648cb506dec4437c8a0bf029
Isaac Pang <isaac.pangth@gmail.com>
2024-07-16 02:11:05 +0800

    added validity checks for start time and end time

--- frontend/src/components/Calendar/ScheduleTaskDialog.jsx
+++ frontend/src/components/Calendar/ScheduleTaskDialog.jsx
@@ -8,9 +8,11 @@ import {
   DialogActions,
   Button,
   Stack,
+  Snackbar,
 } from "@mui/material";
 import axios from "axios";
 import DtPicker from "../DtPicker";
+import { isAfter } from "date-fns";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
 
@@ -20,9 +22,17 @@ export default function ScheduleTaskDialog(props) {
   const [task, setTask] = useState(null);
   const [startDateTime, setStartDateTime] = useState(null);
   const [endDateTime, setEndDateTime] = useState(null);
+  const [openInvalidInput, setOpenInvalidInput] = useState(false);
 
   const handleChangeStart = (newValue) => setStartDateTime(newValue);
   const handleChangeEnd = (newValue) => setEndDateTime(newValue);
+
+  const handleClose = () => {
+    onClose();
+    setTask(null);
+    setStartDateTime(null);
+    setEndDateTime(null);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,57 +51,76 @@ export default function ScheduleTaskDialog(props) {
   }, []);
 
   return (
-    <Dialog
-      open={open}
-      onClose={onClose}
-      PaperProps={{
-        component: "form",
-        onSubmit: (event) => {
-          event.preventDefault();
-          if (task !== null && startDateTime !== null && endDateTime !== null) {
-            onClose();
-          }
-        },
-      }}
-    >
-      <DialogTitle>Schedule Task</DialogTitle>
-      <DialogContent>
-        <Stack spacing={3}>
-          <Autocomplete
-            id="task"
-            options={tasks.filter((task) => task.startTime === undefined)}
-            getOptionLabel={(option) => option.title}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="Task" />}
-            value={task}
-            onChange={(event, newValue) => {
-              setTask(newValue);
-            }}
-          />
-          <DtPicker
-            type="DateTime"
-            label="Start"
-            id="startDateTime"
-            name="startDateTime"
-            value={startDateTime}
-            onChange={handleChangeStart}
-            onClear={() => null}
-          />
-          <DtPicker
-            type="DateTime"
-            label="End"
-            id="endDateTime"
-            name="endDateTime"
-            value={endDateTime}
-            onChange={handleChangeEnd}
-            onClear={() => null}
-          />
-        </Stack>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose}>Cancel</Button>
-        <Button type="submit">Schedule Task</Button>
-      </DialogActions>
-    </Dialog>
+    <>
+      <Dialog
+        fullWidth
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          component: "form",
+          onSubmit: (event) => {
+            event.preventDefault();
+            if (
+              task === null ||
+              startDateTime === null ||
+              endDateTime === null ||
+              isAfter(startDateTime, endDateTime)
+            ) {
+              setOpenInvalidInput(true);
+            } else {
+              handleClose();
+            }
+          },
+        }}
+      >
+        <DialogTitle>Schedule Task</DialogTitle>
+        <DialogContent>
+          <Stack spacing={3}>
+            <Autocomplete
+              id="task"
+              options={tasks.filter((task) => task.startTime === undefined)}
+              getOptionLabel={(option) =>
+                option.dueDate === undefined
+                  ? option.title
+                  : option.title + " (Due " + option.dueDate + ")"
+              }
+              renderInput={(params) => <TextField {...params} label="Task" />}
+              value={task}
+              onChange={(event, newValue) => {
+                setTask(newValue);
+              }}
+            />
+            <DtPicker
+              type="DateTime"
+              label="Start"
+              id="startDateTime"
+              name="startDateTime"
+              value={startDateTime}
+              onChange={handleChangeStart}
+              onClear={() => null}
+            />
+            <DtPicker
+              type="DateTime"
+              label="End"
+              id="endDateTime"
+              name="endDateTime"
+              value={endDateTime}
+              onChange={handleChangeEnd}
+              onClear={() => null}
+            />
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Schedule Task</Button>
+        </DialogActions>
+      </Dialog>
+      <Snackbar
+        open={openInvalidInput}
+        autoHideDuration={5000}
+        onClose={() => setOpenInvalidInput(false)}
+        message="Select a valid task, start time and end time"
+      />
+    </>
   );
 }
