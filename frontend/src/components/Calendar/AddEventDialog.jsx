41602bf0e7a8fae0f25d3d5d413e5b3be5db400a
Isaac Pang <isaac.pangth@gmail.com>
2024-07-17 19:48:41 +0800

    added AddEventDialog component

---
+++ frontend/src/components/Calendar/AddEventDialog.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+import {
+  Autocomplete,
+  TextField,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  Stack,
+  Snackbar,
+} from "@mui/material";
+import axios from "axios";
+import DtPicker from "../DtPicker";
+import { isAfter } from "date-fns";
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+
+export default function AddEventDialog(props) {
+  const { open, onClose } = props;
+  const [startDateTime, setStartDateTime] = useState(null);
+  const [endDateTime, setEndDateTime] = useState(null);
+  const [openInvalidInput, setOpenInvalidInput] = useState(false);
+
+  const handleChangeStart = (newValue) => setStartDateTime(newValue);
+  const handleChangeEnd = (newValue) => setEndDateTime(newValue);
+
+  const handleClose = () => {
+    onClose();
+    setStartDateTime(null);
+    setEndDateTime(null);
+  };
+
+  return (
+    <>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          component: "form",
+          onSubmit: (event) => {
+            event.preventDefault();
+            if (
+              startDateTime === null ||
+              endDateTime === null ||
+              isAfter(startDateTime, endDateTime)
+            ) {
+              setOpenInvalidInput(true);
+            } else {
+              handleClose();
+            }
+          },
+        }}
+      >
+        <DialogTitle>Schedule Task</DialogTitle>
+        <DialogContent>
+          <Stack spacing={3} width={300}>
+            <TextField
+              autoFocus
+              autoComplete="off"
+              required
+              margin="dense"
+              id="title"
+              name="title"
+              label="Event Title"
+              type="text"
+              fullWidth
+              variant="standard"
+            />
+            <DtPicker
+              type="DateTime"
+              label="Start"
+              id="startDateTime"
+              name="startDateTime"
+              value={startDateTime}
+              onChange={handleChangeStart}
+              onClear={() => null}
+              required={true}
+            />
+            <DtPicker
+              type="DateTime"
+              label="End"
+              id="endDateTime"
+              name="endDateTime"
+              value={endDateTime}
+              onChange={handleChangeEnd}
+              onClear={() => null}
+              required={true}
+            />
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Add event</Button>
+        </DialogActions>
+      </Dialog>
+      <Snackbar
+        open={openInvalidInput}
+        autoHideDuration={5000}
+        onClose={() => setOpenInvalidInput(false)}
+        message="Input a valid event title, start time and end time"
+      />
+    </>
+  );
+}
