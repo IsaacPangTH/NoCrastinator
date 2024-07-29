fd52e5932071879462cc12d22620071e74bdec48
ckclion <118067503+ckclion@users.noreply.github.com>
2024-07-29 10:01:49 +0800

    fixed bug in readlink endpoint

--- backend/src/accounts/readlink.js
+++ backend/src/accounts/readlink.js
@@ -10,7 +10,7 @@ const readlink = async (data) => {
   await client.connect();
   try {
     const res = await client.query(`SELECT link FROM accounts WHERE id=$1`, [data.user]);
-    return res.rows[0].link;
+    return res.rows[0] ? res.rows[0].link : "";
   } catch (err) {
     console.error(err);
     return "Error! Please try again later.";
